e13df174c06b0e8b7c6d1675f5d331cd4f1b94ec
Sam Trafton <samatrafton@gmail.com>
2024-01-23 13:06:54 +0200

    mod 2 commits

--- fallow-rain/src/fallow-rain.cpp
+++ fallow-rain/src/fallow-rain.cpp
@@ -7,11 +7,37 @@
 //============================================================================
 
 #include <iostream>
-#include "person.h"
+#include <string>
+#include <limits>
 using namespace std;
+string getString() {
+	// Dynamically allocated array chars
+	// string objects do not require null terminators
+	string s1;
+	string s2;
+	cout << "Please enter in your first string." << endl;
+	// the getline() method includes spaces and consumes the newline char
+	getline(cin, s1);
+	cout << "Please enter in your second string." << endl;
+	getline(cin, s2);
+
+
+
+	return s1 + s2;
+
+}
+
+
 int main(int argc, char **argv) {
 
-	Person person = Person("John", "Smuth", "1234 Foo", "Barston", "1235");
+	// Iterating loop to run the program three times.
+	for (int i = 0; i < 3; i++) {
+		// Clear the buffer before reading a new string to make sure nothing lingers for the next call.
+		if (i != 0) {
+			cin.ignore(numeric_limits<streamsize>::max(), '\n');
+		}
+		cout << getString() << endl;
+	}
 
-	person.displayData();
+	return 0;
 }
